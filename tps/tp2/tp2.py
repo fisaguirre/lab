191567aee8b6267c973a4626ce5247f023269024
Fernando <feragustin-13@hotmail.com>
2019-06-09 23:34:37 -0300

    pylint

--- tps/tp2/tp2.py
+++ tps/tp2/tp2.py
@@ -12,13 +12,13 @@ def a_bordo():
         hincha = random.choice(lista_de_hinchas)
         lista_de_hinchas.remove(hincha)
 
-        if(hincha == 'R'):
+        if hincha == 'R':
                 print("vamos river")
         else:
                 print("vamos boca")
         lista_bote.append(hincha)
 
-        if( (len(lista_bote)) == 4 ):
+        if len(lista_bote) == 4:
                 a_remar()
         #if( len(lista_bote) == 4 ):
         #        capitan = threading.Thread(target=a_remar())
@@ -27,19 +27,16 @@ def a_bordo():
 def a_remar():
         print("----------------")
         print("Empiezan a remar")
-        while( True ):
+        while True:
                 lista_bote.pop()
-                print("hola")
-                if( (len(lista_bote)) == 0 ):
+                if len(lista_bote) == 0:
                         break;
 
 def hincha_river():
-    #print("vamos river")
     lista_de_hinchas.append('R')
     a_bordo()
 
 def hincha_boca():
-    #print("vamos boca")
     lista_de_hinchas.append('B')
     a_bordo()
 
@@ -48,8 +45,8 @@ def barra_brava_river(cantidad_hinchas):
     viajes = 0
     while viajes < cantidad_hinchas:
         #time.sleep(random.randrange(0, 5))
-        r = threading.Thread(target=hincha_river)
-        r.start()
+        river = threading.Thread(target=hincha_river)
+        river.start()
         viajes = viajes + 1
 
 def barra_brava_boca(cantidad_hinchas):
@@ -57,19 +54,17 @@ def barra_brava_boca(cantidad_hinchas):
     viajes = 0
     while viajes < cantidad_hinchas:
         #time.sleep(random.randrange(0, 5))
-        b = threading.Thread(target=hincha_boca)
-        b.start()
+        boca = threading.Thread(target=hincha_boca)
+        boca.start()
         viajes = viajes + 1
 
-t1 = threading.Thread(target=barra_brava_river, args=(8,))
+barra_boca = threading.Thread(target=barra_brava_river, args=(8,))
 t2 = threading.Thread(target=barra_brava_boca, args=(8,))
 
-t1.start()
+barra_boca.start()
 t2.start()
 
-t1.join()
+barra_boca.join()
 t2.join()
-print("la cola quedo: ",lista_de_hinchas)
-
 
 print("terminaron los viajes ")
