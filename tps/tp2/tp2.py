5f770ce7bf60643276f9d4dd48e953d43848724f
Fernando <feragustin-13@hotmail.com>
2019-06-09 23:07:48 -0300

    hacer sincronizacion

--- tps/tp2/tp2.py
+++ tps/tp2/tp2.py
@@ -10,12 +10,28 @@ lista_de_hinchas = []
 
 def a_bordo():
         hincha = random.choice(lista_de_hinchas)
+        lista_de_hinchas.remove(hincha)
+
         if(hincha == 'R'):
                 print("vamos river")
         else:
                 print("vamos boca")
         lista_bote.append(hincha)
-        print("el bote queda: ",lista_bote)
+
+        if( (len(lista_bote)) == 4 ):
+                a_remar()
+        #if( len(lista_bote) == 4 ):
+        #        capitan = threading.Thread(target=a_remar())
+        #print("el bote queda: ",lista_bote)
+
+def a_remar():
+        print("----------------")
+        print("Empiezan a remar")
+        while( True ):
+                lista_bote.pop()
+                print("hola")
+                if( (len(lista_bote)) == 0 ):
+                        break;
 
 def hincha_river():
     #print("vamos river")
@@ -27,26 +43,26 @@ def hincha_boca():
     lista_de_hinchas.append('B')
     a_bordo()
 
-def barra_brava_river():
+def barra_brava_river(cantidad_hinchas):
     """ Generacion de hinchas de River"""
-    asd = 0
-    while asd < 20:
+    viajes = 0
+    while viajes < cantidad_hinchas:
         #time.sleep(random.randrange(0, 5))
         r = threading.Thread(target=hincha_river)
         r.start()
-        asd = asd + 1
+        viajes = viajes + 1
 
-def barra_brava_boca():
+def barra_brava_boca(cantidad_hinchas):
     """ Generacion de hinchas de Boca"""
     viajes = 0
-    while viajes < 20:
+    while viajes < cantidad_hinchas:
         #time.sleep(random.randrange(0, 5))
         b = threading.Thread(target=hincha_boca)
         b.start()
         viajes = viajes + 1
 
-t1 = threading.Thread(target=barra_brava_river)
-t2 = threading.Thread(target=barra_brava_boca)
+t1 = threading.Thread(target=barra_brava_river, args=(8,))
+t2 = threading.Thread(target=barra_brava_boca, args=(8,))
 
 t1.start()
 t2.start()
@@ -54,5 +70,6 @@ t2.start()
 t1.join()
 t2.join()
 print("la cola quedo: ",lista_de_hinchas)
+
 
 print("terminaron los viajes ")
