92c31368ead4be536255858db958806cc8ab194b
Fernando <feragustin-13@hotmail.com>
2019-06-10 23:19:00 -0300

    listo la sincronizacion de remar y esperar

--- tps/tp2/tp2.py
+++ tps/tp2/tp2.py
@@ -10,18 +10,27 @@ lista_de_hinchas = []
 condition = threading.Condition()
 
 def a_bordo(hincha):
-        hincha = random.choice(lista_de_hinchas)
-        lista_de_hinchas.remove(hincha)
-        hincha_a_subir = lista_de_hinchas.pop(hincha)
+        #hincha = random.choice(lista_de_hinchas)
+        #lista_de_hinchas.remove(hincha)
+        hincha_a_subir = lista_de_hinchas.pop()
+        #print("se saco un: ",hincha_a_subir)
 
         if hincha_a_subir == 'R':
                 print("vamos river")
         else:
                 print("vamos boca")
+
         lista_bote.append(hincha_a_subir)
+        print("el bote es: ",lista_bote)
+        #condition.acquire()
 
         if len(lista_bote) == 4:
                 a_remar()
+        condition.release()
+                #condition.notify()
+                #time.sleep(1)
+        #condition.release()
+
         #if( len(lista_bote) == 4 ):
         #        capitan = threading.Thread(target=a_remar())
         #print("el bote queda: ",lista_bote)
@@ -30,26 +39,45 @@ def a_remar():
         print("----------------")
         print("Empiezan a remar")
         while True:
-                lista_bote.pop()
+                #condition.acquire()
+                #condition.wait()
+                for personas in range(4):
+                        print("se saca del bote: ",lista_bote.pop())
                 if len(lista_bote) == 0:
-                        break;
+                        break
+                #condition.release()                        
 
 def hincha_river():
-    #lista_de_hinchas.append('R')
-    #a_bordo()
-    while True:
-            condition.acquire()
-            hincha = 'R'
-            lista_de_hinchas.append(hincha)
-            a_bordo(hincha)
+    condition.acquire()
+    hincha = "R"
+    lista_de_hinchas.append(hincha)
+    a_bordo(hincha)
+    if len(lista_bote) == 4:
+            print("hay 4")
+            condition.wait()
+    #while True:
+            #condition.acquire()
+            #hincha = "R"
+            #lista_de_hinchas.append(hincha)
+            #a_bordo(hincha)
+            #if len(lista_bote) == 4:
+            #        condition.wait()
 
 def hincha_boca():
-    #lista_de_hinchas.append('B')
-    #a_bordo()
-    while True:
-            hincha = 'B'
-            lista_de_hinchas.append(hincha)
-            a_bordo(hincha)
+    condition.acquire()
+    hincha = "B"
+    lista_de_hinchas.append(hincha)
+    a_bordo(hincha)
+    if len(lista_bote) == 4:
+            print("hay 4")
+            condition.wait()
+    #while True:
+    #        condition.acquire()
+    #        hincha = "B"
+    #        lista_de_hinchas.append(hincha)
+    #        a_bordo(hincha)
+    #        if len(lista_bote) == 4:
+    #                condition.wait()
 
 def barra_brava_river(cantidad_hinchas):
     """ Generacion de hinchas de River"""
@@ -69,29 +97,14 @@ def barra_brava_boca(cantidad_hinchas):
         boca.start()
         viajes = viajes + 1
 
-barra_boca = threading.Thread(target=barra_brava_river, args=(8,))
-t2 = threading.Thread(target=barra_brava_boca, args=(8,))
+barra_river = threading.Thread(target=barra_brava_river, args=(8,))
+barra_boca = threading.Thread(target=barra_brava_boca, args=(8,))
 
+barra_river.start()
 barra_boca.start()
-t2.start()
 
+barra_river.join()
 barra_boca.join()
-t2.join()
 
 print("terminaron los viajes ")
 
-
-lista = ['1','2','3','4']
-
-def hola(var):
-        print("var es: ",var)
-        print("lo voy a sacar")
-        nuevo = lista.pop(int(var))
-        print("el nuevo es: ",nuevo)
-
-var = '4'
-lista.append(var)
-print("var:",var)
-print("lista:",lista)
-hola(var)
-print("quedo: ",lista)
