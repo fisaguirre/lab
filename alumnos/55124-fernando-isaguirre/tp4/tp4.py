d712cc30fbe2513821ca30e3b3a2e212fcaf03cf
Fernando <feragustin-13@hotmail.com>
2019-11-03 00:13:02 -0300

    opciones

--- alumnos/55124-fernando-isaguirre/tp4/tp4.py
+++ alumnos/55124-fernando-isaguirre/tp4/tp4.py
@@ -1,50 +1,70 @@
 #!/usr/bin/python3
 import argparse
 import requests
-import re
 import urllib.request, urllib.parse
 from PIL import Image
 from bs4 import BeautifulSoup
-import random
+import urllib.error
+from urllib.request import urlopen
+import sys
+import getopt
 
-# Accept arguments
-argumentParser = argparse.ArgumentParser()
-argumentParser.add_argument("-u", "--url", dest = "urls", nargs = "+", required = True, help = "URL's to be fetched")
-args = argumentParser.parse_args()
+opciones, argumentos = getopt.getopt(sys.argv[1:], "u:h")
+
+#Bucle para recorrer el arreglo y localizar el url de la página.
+URLs_images = ""
+
+for o in opciones:
+    if o[0] == "-h":
+        #Llamada a función de ayuda para guiar al usuario.
+        OpcAyuda()
+    if o[0] == "-u":
+        #La pos. 1 del arreglo constituye la URL de la página.
+        URLs_images = o[1]
+        print(o[1])
 
 # Auxiliar variables
-hosts = args.urls
+urls = URLs_images
+
+hosts = urls.split(",")
+
 contents = {}
 imageSources = []
 
 # Get hosts HTML (using Beautiful Soup)
 for host in hosts:
     contents[host] = BeautifulSoup(requests.get(host).text, "html.parser")
+    #beautifulsoup extrae datos desde html
 
 # Extract images
 for host in hosts:
+    print("el host es: ",host)
     images = contents[host].findAll("img")
     for img in images:
         imgUrl = img.get("src")
+        print("mia es: ",imgUrl)
         if imgUrl[0] == "/":
+            #esto es para los que le falta el hostname
             imgUrl = host + imgUrl
+            print("segunda es: ",imgUrl)
         imageSources.append(imgUrl)
 
 for i in range(len(imageSources)):
     src = imageSources[i]
     #print(imageSources[i])
     parsedUrl = urllib.parse.urlparse(src)
-    newUrl = parsedUrl.scheme + "://www." + parsedUrl.netloc + parsedUrl.path
+    #esto es para parsear las urls de la lista, o sea, devuelve los elementos dela estructura de la url,scheme,netloc,path...
+    newUrl = parsedUrl.scheme + "://" + parsedUrl.netloc + parsedUrl.path
     imageSources[i] = newUrl
 
-lista = []
-for item in imageSources:
-    print("me queda: ",item)
-    
+for url in imageSources:
+    cantidad = len(url)
+    print("la url es: ",url)
+    if (url[cantidad-3::] == "jpg" or url[cantidad-3::] == "png"):
+        try:
+            urllib.request.urlretrieve(url, '/home/fernando/Desktop/Compu2/lab/alumnos//55124-fernando-isaguirre/tp4/images/imagen.jpg')
+        except urllib.error.HTTPError as e:
+            print('status', e.code)
+            print('reason', e.reason)
 
-
-url_imagen = "https://www.www.lavanguardia.com/r/GODO/LV/p6/WebSite/2019/04/02/Recortada/img_lteixidor_20180727-125926_imagenes_lv_colaboradores_lteixidor_gato_2570_26-300-kLmH--656x656@LaVanguardia-Web.jpg" # El link de la imagen
-nombre_local_imagen = "go.jpg" # El nombre con el que queremos guardarla
-imagen = requests.get(url_imagen).content
-with open(nombre_local_imagen, 'wb') as handler:
-	handler.write(imagen)
+#agregar para ponerle nombres a las imagenes
