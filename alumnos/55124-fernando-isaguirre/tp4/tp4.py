e9825a436a5c90ae4e07266098a71705662feb2f
Fernando <feragustin-13@hotmail.com>
2019-11-29 01:04:47 -0300

    el hilo se queda bloqueado al quere descargar una imagen usando asincronismo

--- alumnos/55124-fernando-isaguirre/tp4/tp4.py
+++ alumnos/55124-fernando-isaguirre/tp4/tp4.py
@@ -13,6 +13,52 @@ import os
 import crear_rojo
 import crear_verde
 import crear_azul
+import multiprocessing as mp
+import time
+#el unico problema es al querer descargar la imagen con urlretrieve.... en asincronismo
+def descarga_conversion(url,nombre_imagen,extension):
+    print("se inicia un hijo: ",os.getpid())
+    ruta = 'images/'
+    ruta_imagen = ruta+nombre_imagen+extension
+    print("la ruta es: ",ruta_imagen)
+    if (extension == ".jpg" or extension == ".png"):
+        print("extension : ",extension,"con proceso: ",os.getpid())
+        #donde empieza
+        #cantidad de caracteres
+        #cada tantos caracteres
+        try:
+            print("entra al try")
+            urllib.request.urlretrieve(url, ruta_imagen)
+            #hay un problema al descargar las imagenes con asincronismo
+            #nomb = "blank"
+            #ex = ".jpg"
+            print("1")
+            im = Image.open(ruta_imagen).convert('RGB').save(ruta+nombre_imagen+".ppm")
+            #im = Image.open(ruta+nomb+ex).convert('RGB').save(ruta+nomb+".ppm")
+            print("2")
+            os.remove(ruta_imagen)
+            #convert('rgb') sirve porque sino salta un error para convertirla a ppm
+            crear_rojo.rojo(ruta+nombre_imagen)
+            #crear_rojo.rojo(ruta+nomb)
+            print("3")
+            print("4")
+            crear_azul.azul(ruta+nombre_imagen)
+            #crear_azul.azul(ruta+nomb)
+            print("4")
+            crear_verde.verde(ruta+nombre_imagen)
+            #crear_verde.verde(ruta+nomb)
+            print("5")
+        except urllib.error.HTTPError as e:
+            print('status', e.code)
+            print('reason', e.reason)
+        print("termina")
+    else:
+        print("es otra")
+    print("termina hijo")
+    return 
+
+#def call(retorno):
+#    print("se devuelve: ",retorno)
 
 opciones, argumentos = getopt.getopt(sys.argv[1:], "u:h")
 
@@ -55,40 +101,21 @@ for i in range(len(imageSources)):
     imageSources[i] = newUrl
 
 ruta = 'images/'
-lista_numeros_imagenes = []
 
-for i in range(len(imageSources)):
-    lista_numeros_imagenes.append(i)
-
+pool = mp.Pool(4)
+start_time = time.time()
 for url in imageSources:
-    cantidad = len(url)
-    if (url[cantidad-3::] == "jpg"):
-        #donde empieza
-        #cantidad de caracteres
-        #cada tantos caracteres
-        try:
-            nombre_imagen= lista_numeros_imagenes.pop()
-            urllib.request.urlretrieve(url, ruta+str(nombre_imagen)+".jpg")
-            ruta_imagen = 'images/'+str(nombre_imagen)
-            im = Image.open(ruta_imagen+".jpg").convert('RGB').save(ruta_imagen+".ppm")
-            os.remove(ruta_imagen+".jpg")
-            #convert('rgb') sirve porque sino salta un error para convertirla a ppm
-            crear_rojo.rojo(ruta_imagen)
-            crear_azul.azul(ruta_imagen)
-            crear_verde.verde(ruta_imagen)
-        except urllib.error.HTTPError as e:
-            print('status', e.code)
-            print('reason', e.reason)
-    if (url[cantidad-3::] == "png"):
-        try:
-            nombre_imagen= lista_numeros_imagenes.pop()
-            urllib.request.urlretrieve(url, ruta+str(nombre_imagen)+'.png')
-            ruta_imagen = 'images/'+str(nombre_imagen)
-            im = Image.open(ruta_imagen+".png").convert('RGB').save(ruta_imagen+".ppm")
-            os.remove(ruta_imagen+".png")
-            crear_rojo.rojo(ruta_imagen)
-            crear_azul.azul(ruta_imagen)
-            crear_verde.verde(ruta_imagen)
-        except urllib.error.HTTPError as e:
-            print('status', e.code)
-            print('reason', e.reason)
+    contador = 0
+    url_split = url.split("/")
+    longitud_url_split = len(url_split)
+    for split in url_split:
+        contador = contador + 1
+        if (contador == longitud_url_split):
+            nombre_imagen = split[:len(split)-4:]
+            extension = split[-4::]
+            print(".")
+            #descarga_conversion(url,nombre_imagen,extension)
+            pool.apply_async(descarga_conversion, args=(url,nombre_imagen,extension,))
+            #el unico que no funciona es el pool.apply_async
+elapsed_time = time.time() - start_time
+print("tiempo total: ",elapsed_time-start_time)
